7451ea5ee53f09ee21d868b45f9d3c50037df713
zevene88 <zeven0067@gmail.com>
2025-02-13 15:07:06 +0700

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function nextPage() {
 // Function to move the "No" button when clicked
 function moveButton() {
     const noButton = document.getElementById("noButton");
-    const randomX = Math.floor(Math.random() * 200) - 100; // Random X offset
-    const randomY = Math.floor(Math.random() * 200) - 100; // Random Y offset
+    const randomX = Math.floor(Math.random() * 500) - 150; // Random X offset
+    const randomY = Math.floor(Math.random() * 500) - 150; // Random Y offset
     noButton.style.transform = `translate(${randomX}px, ${randomY}px)`;
 }
